be2a5f1c43160052591f688cce08fb0828838180
Aruneem Bhowmick <aruneem.bhowmick@gmail.com>
2026-03-25 19:43:36 -0500

    refactor: wire bot initialization into entry point

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 import "dotenv/config";
-import pino from "pino";
+import { bot } from "./bot/bot.js";
 
-const logger = pino({ level: process.env.LOG_LEVEL ?? "info" });
-
-logger.info("Thread starting...");
+bot.start();
